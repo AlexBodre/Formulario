9b2552095c9805be444488f16f0fa9caf1e6616a
Alex Bodre <aboordonez@gmail.com>
2019-03-31 12:01:37 -0400

    Hola again

---
+++ Helpers/Helpers/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Helpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Helpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult mostrar( Class1 objeto, FormCollection collection)
+        {
+            string pasatiempos = collection["Pasatiempos"];
+
+            objeto.pasatiempos = pasatiempos;
+
+            return View(objeto);
+        }
+
+
+        public ActionResult Formulario()
+        {
+            List<string> pasatiempos = new List<string>();
+
+            pasatiempos.Add("Deportes");
+            pasatiempos.Add("Programar");
+            pasatiempos.Add("Ver tv");
+            pasatiempos.Add("Dormir");
+
+
+            ViewBag.pasatiempo = pasatiempos;
+            return View();
+        }
+    }
+}
---
+++ Helpers/Helpers/Models/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Helpers.Models
+{
+    public class Class1
+    {
+        [Required]
+        public string cedula { get; set; }
+        [Required]
+        public string nombre { get; set; }
+        [Required]
+        public string apellido { get; set; }
+        [Range(15, double.MaxValue, ErrorMessage = "Debes tener al menos 15 años")]
+        public int edad { get; set; }
+        public string telefono { get; set; }
+        [EmailAddress]
+        public string correo { get; set; }
+        public string genero { get; set; }
+        public string estadoCivil { get; set; }
+        public string pasatiempos { get; set; }
+    }
+}
